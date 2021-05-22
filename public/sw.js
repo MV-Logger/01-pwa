8330def52e539c9da32c99ac9e56b01152a20b0f
maarten.vercruysse <maarten.vercruysse@student.howest.be>
2021-05-22 15:49:03 +0200

    Update the cached urls

--- public/sw.js
+++ public/sw.js
@@ -1,33 +1,35 @@
 const CACHE_NAME = "logs-v1";
 
 const CACHED_URLS = [
-  "/",
-  "/index.html",
-  "/assets/css/bootstrap.min.css",
-  "/assets/css/form.css",
-  "/assets/css/home.css",
-  "/assets/js/libs/boostrap.bundle.min.js",
-  "/assets/js/libs/localforage.min.js",
-  "/assets/js/home.js",
-  "/assets/js/login.js",
-  "/assets/js/login.js",
-  "/assets/js/register.js",
-  "/assets/js/main.js"
+    "/",
+    "/index.html",
+    "/register.html",
+    "/home.html",
+    "/assets/css/bootstrap.min.css",
+    "/assets/css/form.css",
+    "/assets/css/home.css",
+    "/assets/js/libs/bootstrap.bundle.min.js",
+    "/assets/js/libs/localforage.min.js",
+    "/assets/js/home.js",
+    "/assets/js/login.js",
+    "/assets/js/register.js",
+    "/assets/js/main.js",
+    "https://fonts.googleapis.com/css?family=Nunito"
 ]
 
-self.addEventListener("install", function(e) {
-  e.waitUntil(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.addAll(CACHED_URLS);
-      })
-  )
+self.addEventListener("install", function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(CACHED_URLS);
+        })
+    )
 })
 
-self.addEventListener("fetch", function(e) {
-  e.respondWith(fetch(e.request)
-      .catch(e => {
-        return caches.open(CACHE_NAME).then(cache => {
-          return cache.match(e.request);
-        })
-      }));
+self.addEventListener("fetch", function (e) {
+    e.respondWith(fetch(e.request)
+        .catch(e => {
+            return caches.open(CACHE_NAME).then(cache => {
+                return cache.match(e.request);
+            })
+        }));
 });
